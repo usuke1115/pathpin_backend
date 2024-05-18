7f47d55f1338540f9084c785f06868ed489a4e75
yuusuke <xiangzhongyoufu@gmail.com>
2024-05-19 04:01:42 +0900

    update: created_atとupdated_atの型を変更

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,15 +4,15 @@ from django.db import models
 class Folders(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    created_at = models.CharField(max_length=256)
-    updated_at = models.CharField(max_length=256)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
 class Places(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=256)
-    image_url = models.CharField(max_length=256)    # image_url = models.URLField(max_length=1024, blank = True)
-    memo = models.CharField(max_length=256)   # memo = models.TextField(blank=True)
-    created_at = models.CharField(max_length=256)    # create_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.CharField(max_length=256)   # updated_at = models.DateTimeField(auto_now=Ture)
+    image_url = models.CharField(max_length=256)
+    memo = models.CharField(max_length=256)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
     folder_id = models.ForeignKey('Folders', on_delete=models.CASCADE, default='')
