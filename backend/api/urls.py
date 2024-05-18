589291bd085aa8b90b9e8a3d5b2aa4cc3f4c8ba9
yuusuke <xiangzhongyoufu@gmail.com>
2024-05-19 00:17:25 +0900

    fix: エンドポイントのエラーを解決

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('folders/', views.FoldersView.as_view()),
-    path('folders/<int:folder_id>', views.FoldersViewId.as_view()),
+    path('folders/<int:folder_id>/', views.FoldersViewId.as_view()),
     path('places/', views.PlacesView.as_view()),
     path('places/<int:place_id>', views.PlacesViewID.as_view()),
 ]
