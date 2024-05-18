d4ed6846c604249c06caa35fa73571d79e83769c
Motomoto99 <motoshou39@icloud.com>
2024-05-18 20:38:38 +0900

    処理の作成

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,30 +8,61 @@ class PlacesView(APIView):
     # 場所に関する関数
     def get(self,request,format=None):
         # 場所の一覧を取得する、一覧取得
-        return Response(status.HTTP_200_OK)
-
+        places = Places.objects.all()
+        serializers = PlacesSerializer(places,many=True)
+        return Response(serializers.data,status.HTTP_200_OK)
+    
     def post(self,request,format=None):
         # 新しい場所の作成
-        pass
-
+        serializer = PlacesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Successfully created a new location!"},HTTP_200_OK)
+        return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
 
 
 class PlacesViewID(APIView):
     # 場所のIDを使用した関数
-
+    def get(self,request,place_id):
         # IDで指定した場所を取得
-        pass
-
-
-    
-    def put(self,request,id,format=None):
-        # IDで指定したの（場所の名前の変更 or メモを変更　or 住所を変更　or 画像を変更 or updated_atに日付を追加）
+        try:
+            place = Places.objects.get(id=place_id)
+            serializer = PlacesSerializer(place)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status.HTTP_200_OK)
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        except Places.DoesNotExist:
+            return Response(status.HTTP_404_NOT_FOUND)
+   
+    def put(self,request,place_id):
+        # IDで指定したの（場所の名前の変更 or メモを変更 or 住所を変更 or 画像を変更 or updated_atに日付を追加）
         # 変更した項目ごとにHTTPレスポンスの内容を変更する
+        try:
+            place = Places.objects.get(id=place_id)
+            serializer = PlacesSerializer(place, data=request.data)
+            if serializer.is_valid:
+                # リクエストのデータに含まれるフィールドを更新
+                if 'name' in data:
+                    place.name = data['name']
+                if 'memo' in data:
+                    place.memo = data['memo']
+                if 'address' in data:
+                    place.address = data['address']
+                if 'image_url' in data:
+                    place.image_url = data['image_url']
+                if 'updated_at' in data:
+                    place.updated_at = data['updated_at']
+                place.save()
+                serializer = PlacesSerializer(place)
+                return Response(serializer.data,status.HTTP_200_OK)
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        except Places.DoesNotExist:
+            return Response(status.HTTP_404_NOT_FOUND)
 
-    
-    def delete(self,request,id,format=None):
+    def delete(self,request,place_id):
         # IDで指定した場所を削除
-    try:
+        try:
             place = Places.objects.get(id=place_id)
             place.delete()
             return Response(status.HTTP_204_NO_CONTENT)
