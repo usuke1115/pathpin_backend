139c73a01e780c9ba241769d448cda05aff8f55d
Motomoto99 <motoshou39@icloud.com>
2024-05-19 12:20:41 +0900

    パスの調整

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('folders/', views.FoldersView.as_view()),
     path('folders/<int:folder_id>/', views.FoldersViewId.as_view()),
     path('places/', views.PlacesView.as_view()),
-    path('places/<int:place_id>', views.PlacesViewID.as_view()),
+    path('places/<int:place_id>/', views.PlacesViewID.as_view()),
 ]
